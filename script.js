fb1ecf0d6fc0194834564df09dae6a5542f6f736
Emre Ay <90474810+emreay17@users.noreply.github.com>
2021-10-22 01:30:24 +0300

    js added

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputText =document.getElementById('inputText');
+let clearToDoButton = document.getElementById('clearToDo');
 
 addToDoButton.addEventListener('click', function(){
 
-    let list = document.createElement('li');
+    let list = document.createElement('p');
+    toDoContainer.classList.add('list-style');
     toDoContainer.appendChild(list);
     list.innerHTML = inputText.value;
     inputText.value = '';
-
+    clearToDoButton.addEventListener('click', function(){
+    list.style.display='none';
+    })
+   
 })
-
